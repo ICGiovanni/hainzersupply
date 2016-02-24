a9d178181e9c2df652fe9b5d4b9c9bc19782ac9c
Giovanni <Giovanni@DESKTOP-4MH5HFI>
2016-02-23 21:26:47 -0600

    Merge origin/master
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 echo "test auto pull server";
-echo "test auto pull server 2";
-echo "test auto pull server 3";
-echo "test auto pull server 4";
-echo "test auto pull server 5";
-echo "test auto pull server 6";
