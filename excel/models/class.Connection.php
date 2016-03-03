663ea8793ba89fed7279c3e82310c44036adf94f
uriel.medina <uriel.medina>
2016-03-03 13:02:06 -0600

    Connect

--- excel/models/class.Connection.php
+++ excel/models/class.Connection.php
@@ -6,7 +6,7 @@ class Connection
 	public $db;
 	
 	//function __construct($host='127.0.0.1', $dbname='xsmarket_hainzersupply', $user='xsmarket_hainzer',$pass='frIgVCSibswZ9BnZ')
-	function __construct($host='db614233235.db.1and1.com', $dbname='dbo614233235', $user='dbo614233235',$pass='Desarrollo2016*')
+	function __construct($host='db614233235.db.1and1.com', $dbname='db614233235', $user='dbo614233235',$pass='Desarrollo2016*')
 	{
         $this->dbhost = $host;
         $this->dbname = $dbname;
