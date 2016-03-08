002810b71fbab945a800ea1e787c0a83a3a8384d
Giovanni <vagio12345@gmail.com>
2016-03-08 07:38:56 -0600

    Rejected commit: Default

--- index.php
+++ index.php
@@ -1,2 +1,37 @@
-<?php
-echo "Index";
+<html>
+<head>
+    <title>Administración de inventario</title>
+    <meta charset="UTF-8">
+
+    <script src="distribuidores/js/jquery.min.js"></script>
+    <script src="distribuidores/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="distribuidores/css/bootstrap.min.css">
+    <link rel="stylesheet" href="distribuidores/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="distribuidores/css/styles-distribuidores.css">
+
+</head>
+    <body>
+
+        <div class="container">
+            <ul class="nav navbar-nav navbar navbar-default navbar-fixed-top">
+                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">Link</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+
+    </body>
+</html>
+
