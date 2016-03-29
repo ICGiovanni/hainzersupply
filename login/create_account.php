a33605fcab5941572c7d27f5ed508f4229f8b9ac
Giovanni <vagio12345@gmail.com>
2016-03-28 19:09:42 -0600

    LogotipoHainzer

--- login/create_account.php
+++ login/create_account.php
@@ -11,6 +11,28 @@ if(!empty($_POST["create_user"])){
 
 	$user_id = $login->sign_up($firstName, $lastName, $profile, $email, $password);
 
+
+    $to = $email;
+    $subject = "Registro Hainzersupply";
+    $txt = "<html>
+                <body style='background-color: #000'>
+                    <p><img src='' alt='HainzerSupply'/></p>
+                    <table>
+                        <tr>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        </tr>
+                        <tr>
+                        <td>John</td>
+                        <td>Doe</td>
+                        </tr>
+                    </table>
+                </body>
+            </html>";
+    $headers = "From: ventas@hainzersupply.com \r\n";
+    mail($to,$subject,$txt,$headers);
+
+
 	if(isset($_POST['registroExterno'])){
 		header('location: index.php');
 	}else{
