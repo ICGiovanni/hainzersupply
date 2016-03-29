e7e47bd2de274c03c3944e7f0529c7b6c4e9fe8f
Spawn <umedina86@yahoo.com.mx>
2016-03-29 01:23:54 -0600

    Log

--- excel/models/class.Log.php
+++ excel/models/class.Log.php
@@ -40,8 +40,58 @@ class Log
 		$this->CleanLog();
 	}
 	
+	public function GetDataTable($table,$nameFile)
+	{
+		$sql="SELECT *
+				FROM $table";
+		
+		$statement=$this->connect->prepare($sql);
+		$statement->execute();
+		$result=$statement->fetchAll(PDO::FETCH_ASSOC);
+		
+		$content="";
+		foreach($result as $r)
+		{
+			$values="";
+			$cont=0;
+			foreach($r as $k=>$c)
+			{
+				
+				if($k=='post_content' || $k=='post_excerpt')
+				{
+					$c=mysql_escape_string($c);
+				}
+				
+				if($cont==0)
+				{
+					$values.="'$c'";
+				}
+				else
+				{
+					$values.=",'$c'";
+				}
+				$cont++;
+			}
+			$content.="INSERT INTO $table VALUES($values);\n";
+		}
+		
+		$file=$_SERVER["REDIRECT_PATH_CONFIG"].'/excel/sql/'.$nameFile;
+		$f=fopen($file,"w");
+		fwrite($f,$content);
+		fclose($f);
+	}
+	
 	public function GetRestore($name)
 	{
+		$nameFile=$name.'_'.'wp_posts.sql';
+		$this->GetDataTable('wp_posts',$nameFile);
+		
+		$nameFile=$name.'_'.'wp_postmeta.sql';
+		$this->GetDataTable('wp_postmeta',$nameFile);
+		
+		$nameFile=$name.'_'.'wp_term_relationships.sql';
+		$this->GetDataTable('wp_term_relationships',$nameFile);
+		/*
 		$nameFile=$name.'_'.'wp_posts.sql';
 		$file=$_SERVER["REDIRECT_PATH_CONFIG"].'/excel/sql/'.$nameFile;
 		$sql="SELECT * INTO OUTFILE '$file' FROM wp_posts";
@@ -58,12 +108,43 @@ class Log
 		$file=$_SERVER["REDIRECT_PATH_CONFIG"].'/excel/sql/'.$nameFile;
 		$sql="SELECT * INTO OUTFILE '$file' FROM wp_term_relationships";
 		$statement=$this->connect->prepare($sql);
+		$statement->execute();*/
+	}
+	
+	public function RestoreDataTable($table,$nameFile)
+	{
+		$sql="DELETE FROM $table";
+		$statement=$this->connect->prepare($sql);
 		$statement->execute();
+		
+		$file=$_SERVER["REDIRECT_PATH_CONFIG"].'/excel/sql/'.$nameFile;
+		
+		$f=fopen($file, "r");
+		
+		while(($sql=fgets($f))!==false)
+		{
+			if($sql)
+			{
+				$statement=$this->connect->prepare($sql);
+				$statement->execute();
+			}
+		}
+		
+		fclose($f);
 	}
 	
 	public function Restore($name)
 	{
-		$sql="DELETE FROM wp_posts";
+		$nameFile=$name.'_'.'wp_posts.sql';
+		$this->RestoreDataTable('wp_posts',$nameFile);
+		
+		$nameFile=$name.'_'.'wp_postmeta.sql';
+		$this->RestoreDataTable('wp_postmeta',$nameFile);
+		
+		$nameFile=$name.'_'.'wp_term_relationships.sql';
+		$this->RestoreDataTable('wp_term_relationships',$nameFile);
+		
+		/*$sql="DELETE FROM wp_posts";
 		$statement=$this->connect->prepare($sql);
 		$statement->execute();
 		
@@ -92,7 +173,7 @@ class Log
 		$file=$_SERVER["REDIRECT_PATH_CONFIG"].'/excel/sql/'.$nameFile;
 		$sql="LOAD DATA INFILE '$file' INTO TABLE wp_term_relationships";
 		$statement=$this->connect->prepare($sql);
-		$statement->execute();
+		$statement->execute();*/
 		
 	}
 	
@@ -160,6 +241,14 @@ class Log
 		
 			$log->DeleteLog($logId);
 		}
+	}
+	
+	function mres($value)
+	{
+		$search = array("\\",  "\x00", "\n",  "\r",  "'",  '"', "\x1a");
+		$replace = array("\\\\","\\0","\\n", "\\r", "\'", '\"', "\\Z");
+	
+		return str_replace($search, $replace, $value);
 	}
 }
 
