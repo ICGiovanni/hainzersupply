b8c2b57cf3a35b2a00546cb95288855e828c8d65
shingoon <shingoo_n@yahoo.com.mx>
2016-04-01 11:13:40 -0600

    Update create_order.php

--- orders/ajax/create_order.php
+++ orders/ajax/create_order.php
@@ -5,6 +5,10 @@ $order = new Order();
 $_POST['inv_orden_compra_status_id'] = '1';
 print_r($_POST);
 
-$order->insertOrder($_POST);
+$idOrder = $order->insertOrder($_POST);
+/**/
+
+/**/
+echo "order_id=".$idOrder;
 }
 ?>
