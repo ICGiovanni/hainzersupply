2599a7b755fe583d893d2cd272b7b9d67b71d9d0
Giovanni <vagio12345@gmail.com>
2016-04-04 00:04:51 -0500

    Quitar CUrl

--- excel/updateProducts.php
+++ excel/updateProducts.php
@@ -1,5 +1,4 @@
 <?php
-include_once $_SERVER['REDIRECT_PATH_CONFIG'].'config.php';
 require_once('models/class.Inventory.php');
 
 $inventory=new Inventory();
@@ -16,9 +15,6 @@ $ID=$inventory->getSku($sku);
 if($ID)
 {
 	echo $inventory->UpdateDataProduct($ID,$name,$stock,$price);
-	$handler = curl_init($ruta."orders/create_json.php");
-	$response = curl_exec ($handler);
-	curl_close($handler);
 }
 else
 {
